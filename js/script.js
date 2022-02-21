93308e4e0f27bcff913425d6946763dcbbf26767
LLPROG <94935580+LLPROG@users.noreply.github.com>
2022-02-21 15:57:11 +0000

    math round

--- js/script.js
+++ js/script.js
@@ -4,14 +4,13 @@ let userName = prompt("come ti chiami?");
 let age = prompt("quanti anni hai?");
 let kilo = prompt("quanti kilometri percorrerai?");
 
-let totPrize = (prize * kilo).toFixed(1);
+let totPrize = prize * kilo;
 
-let discount20 = totPrize * 20 / 100;
-let discount40 = totPrize * 40 / 100;
+let totPrizeF = Math.round(totPrize);
 
-// let disk20 = discount20.toFixed(1); 
-// let disk40 = discount20.toFixed(1); 
-// let totPrizeFinal = discount20.toFixed(1); 
+let discount20 = totPrizeF * 20 / 100;
+let discount40 = totPrizeF * 40 / 100;
+
 
 
 
@@ -19,14 +18,16 @@ let element = document.getElementById("my-id");
 
 
 
-if (age <= 17) {
-    element.innerHTML= `Il prezzo del biglietto é ${totPrize - discount20} euro`
-} else if (age >= 68) {
-    element.innerHTML = `Il prezzo del biglietto é ${totPrize - discount40} euro`
-} else {
-    element.innerHTML = `Il prezzo del biglietto é ${totPrize} euro`
-}
 
+
+
+if (age <= 17) {
+    element.innerHTML= `Il prezzo del biglietto é ${totPrizeF - discount20} euro`
+} else if (age >= 68) {
+    element.innerHTML = `Il prezzo del biglietto é ${totPrizeF - discount40} euro`
+} else {
+    element.innerHTML = `Il prezzo del biglietto é ${totPrizeF} euro`
+}
 
 
 
