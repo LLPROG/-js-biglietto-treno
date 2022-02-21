0a315f32b70f256a64870523389832f1876b81ab
LLPROG <94935580+LLPROG@users.noreply.github.com>
2022-02-21 15:25:55 +0000

    to fixed

--- js/script.js
+++ js/script.js
@@ -4,34 +4,32 @@ let userName = prompt("come ti chiami?");
 let age = prompt("quanti anni hai?");
 let kilo = prompt("quanti kilometri percorrerai?");
 
-let totPrize = prize * kilo;
+let totPrize = (prize * kilo).toFixed(1);
 
 let discount20 = totPrize * 20 / 100;
 let discount40 = totPrize * 40 / 100;
+
+// let disk20 = discount20.toFixed(1); 
+// let disk40 = discount20.toFixed(1); 
+// let totPrizeFinal = discount20.toFixed(1); 
 
 
 
 let element = document.getElementById("my-id");
 
+
+
 if (age <= 17) {
-    element.innerHTML = `Il prezzo del biglietto é ${totPrize - discount20}`
+    element.innerHTML= `Il prezzo del biglietto é ${totPrize - discount20} euro`
 } else if (age >= 68) {
-    element.innerHTML = `Il prezzo del biglietto é ${totPrize - discount40}`
+    element.innerHTML = `Il prezzo del biglietto é ${totPrize - discount40} euro`
 } else {
-    element.innerHTML = `Il prezzo del biglietto é ${totPrize}`
+    element.innerHTML = `Il prezzo del biglietto é ${totPrize} euro`
 }
 
 
 
 
-
-
-
-
-
-
-
-// element.innerHTML = `Ciao ${userName}, il prezzo del biglietto é ${kilo * prize}`;
 
 
    
